ff775f6a52056256b3ea98d5c3c02ba0756fd965
Eric Dalquist <eric.dalquist@gmail.com>
2015-12-11 18:54:24 +0000

    Fix Start

--- bin/start_caves.sh
+++ bin/start_caves.sh
@@ -3,5 +3,6 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $SCRIPT_DIR/dst_config
 
-"$DST_HOME/bin/dontstarve_dedicated_server_nullrenderer" -conf_dir DST_Cave -console
+cd $DST_HOME
+"$DST_HOME/dontstarve_dedicated_server_nullrenderer" -conf_dir DST_Cave -console
 
--- bin/start_overworld.sh
+++ bin/start_overworld.sh
@@ -3,4 +3,5 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 . $SCRIPT_DIR/dst_config
 
-"$DST_HOME/bin/dontstarve_dedicated_server_nullrenderer" -conf_dir DST_Overworld -console
+cd $DST_HOME
+"$DST_HOME/dontstarve_dedicated_server_nullrenderer" -conf_dir DST_Overworld -console
